76d5cd79e56dc9455bdd04250811683a6785c676
Michael Mittelman <michael.mittelman@xandr.com>
2019-09-16 10:10:37 -0700

    Adding base files for yoga server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,16 @@
-// let's go!
+require('dotenv').config({ path: '.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO -> Use middleware to handle cookies (JWT)
+
+server.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    },
+}, details => {
+    console.log(`Server is now running on port http://localhost:${details.port}`);
+});
